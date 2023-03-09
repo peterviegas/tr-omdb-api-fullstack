9156add0bc254ab5d5bd1da924cb3b9b108fe417
glen <glen_dovey@hotmail.com>
2023-03-09 20:41:20 +0000

    feat: added a backend

---
+++ server/src/route/routes.ts
@@ -0,0 +1,7 @@
+import express from 'express'
+import * as movieController from '../controllers/movie_controller'
+
+export const router = express.Router()
+router.get('/', (req, res) => {
+    res.send('Welcome to the Movie API!')
+})
---
+++ server/src/server.ts
@@ -0,0 +1,7 @@
+import { app } from "./app";
+
+const PORT = 3001
+
+app.listen(PORT, () => {
+	console.log(`🚂 Express started on port ${PORT}`);
+});
