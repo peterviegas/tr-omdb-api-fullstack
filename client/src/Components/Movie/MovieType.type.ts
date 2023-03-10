442841daf63a8b4eda6848277791afd704a67b50
neema <neemadas@gmail.com>
2023-03-10 19:51:53 +0000

    fix: Add the changes in OmbdApiFetch function to movieList

--- client/src/Components/Movie/MovieType.type.ts
+++ client/src/Components/Movie/MovieType.type.ts
@@ -1,5 +1,34 @@
-export type MovieCardType = {
+export type MovieResponseType = {
+    Title: string;
+    Year: string;
+    Rated: string;
+    Released: string;
+    Runtime: string;
+    Genre: string;
+    Director: string;
+    Writer: string;
+    Actors: string;
+    Plot: string;
+    Language: string;
+    Country: string;
+    Awards: string;
+    Poster: string;
+    Ratings: string;
+    Metascore: string;
+    imdbRating: string;
+    imdbVotes: string;
+    imdbID: string;
+    Type: string;
+    DVD: string;
+    BoxOffice: string;
+    Production: string;
+    Website: string;
+    Response: boolean;
+}
 
+
+
+export type MovieCardType = {
     Actors: string;
     Awards: string;
     Country: string;
@@ -8,7 +37,7 @@ export type MovieCardType = {
     Language: string;
     Plot: string;
     Poster: string;
-    Rated: number;
+    Rated: string;
     Released: string;
     Runtime: string;
     Title: string;
@@ -16,16 +45,38 @@ export type MovieCardType = {
     Writer: string;
     Year: string;
     imdbID: string;
-    imdbRating: number;
+    imdbRating: string;
     imdbVotes: string;
 }
 
-export type MovieType = {
+export const defaultMovieCard = {
+    Actors: "",
+    Awards: "",
+    Country: "",
+    Director: "",
+    Genre: "",
+    Language: "",
+    Plot: "",
+    Poster: "",
+    Rated: "",
+    Released: "",
+    Runtime: "",
+    Title: "",
+    Type: "",
+    Writer: "",
+    Year: "",
+    imdbID: "",
+    imdbRating: "",
+    imdbVotes: "",
+}
 
+export type MovieType = {
     Title: string;
     Year: string;
     imdbID: string;
     Type: string;
     Poster: string;
 }
+
+
 
