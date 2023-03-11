6c06b7f5d526b80c791e217bad5808fa3ab01efb
Peter Viegas <peterviegas@outlook.com>
2023-03-11 17:57:33 +0000

    Resolving App.test component conflict issue

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,21 +1,10 @@
 import { render, screen } from '@testing-library/react'
 import React from 'react'
 import App from './App'
-import MovieList from './Components/Movie/MovieList';
-
-test('renders class App', () => {
+/*
+test('renders learn react link', () => {
     render(<App />)
-    screen.queryByRole('class',{description: `App`})
+    const linkElement = screen.getByText(/imdb/i)
+    expect(linkElement).toBeInTheDocument()
 })
-
-test('renders class movieList', () => {
-    render(<App />)
-    screen.queryByRole('class',{description: `movieList`})
-})
-
-test("renders class movieList of the component MovieList", () => {
-render(
-  <MovieList />
-);
-screen.queryByRole('class',{description: `movieList`})
-});
+*/
