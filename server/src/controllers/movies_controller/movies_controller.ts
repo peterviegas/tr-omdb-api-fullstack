aba33ca32f3e2eea0ab5d26283e677333ba733ab
glen <glen_dovey@hotmail.com>
2023-03-12 21:17:56 +0000

    test: added api search tests

--- server/src/controllers/movies_controller/movies_controller.ts
+++ server/src/controllers/movies_controller/movies_controller.ts
@@ -5,14 +5,19 @@ import { isString } from '../../util/typeHandlers';
 import { convertUrlToArray } from '../../util/urlHandler';
 
 export const getMovies = async (req: Request, res: Response) => {
-    if (isApiKeyPresent()) false;
+    if (!isApiKeyPresent()) {
+        const error = 'API Key not found';
+        console.log(error);
+        res.status(500).send(error);
+        return;
+    }
     const [title] = convertUrlToArray(req.url);
     if (!title) {
         res.status(400).send('Title not found');
         return false;
     }
     const data = await getMoviesFromApi(title);
-    if (isString(data)) {
+    if (isString(data) || data === undefined) {
         res.status(500).send(data);
         return;
     }
