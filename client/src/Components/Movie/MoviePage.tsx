0c8ac5e071f4b95706b7794d328601f0944bfd77
neema <neemadas@gmail.com>
2023-03-12 10:50:07 +0000

    fix: Change route of moviepage

--- client/src/Components/Movie/MoviePage.tsx
+++ client/src/Components/Movie/MoviePage.tsx
@@ -3,9 +3,12 @@ import { MovieType } from './Type/MovieType.type';
 import { MoviesContext } from './Context/MoviesContext';
 import { RenderMoviesList } from './RenderMoviesList';
 import MovieCard from './MovieCard';
+import { useLocation } from 'react-router-dom';
 const MoviePage: React.FC = () => {
     const [id, setId] = useState<string>('');
+
     const { movies } = useContext(MoviesContext);
+    const location = useLocation();
 
     const unselectedMovies = (id: string): Array<MovieType> => {
         const movieList: Array<MovieType> = movies.filter(
@@ -14,14 +17,21 @@ const MoviePage: React.FC = () => {
         return movieList;
     };
 
+    const onClickHandler = (e: React.MouseEvent) => {
+        e.preventDefault();
+        const { pathname } = location;
+        unselectedMovies(pathname.split('/')[2]);
+        setId(pathname.split('/')[2]);
+    };
+
     return (
         <div className='moviePage'>
-            <MovieCard setId={(id) => setId(id)} />
+            <MovieCard />
             <h3 className='moviePage__heading-3'>
                 Other movies of your search list :
             </h3>
             <div className='moviePage__list'>
-                {RenderMoviesList(unselectedMovies(id), id)}
+                {RenderMoviesList(unselectedMovies(id), onClickHandler)}
             </div>
         </div>
     );
