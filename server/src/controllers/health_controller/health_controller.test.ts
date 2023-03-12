0f61ebbe09ab6b3ee6733d69b51489e8cd9fda66
glen <glen_dovey@hotmail.com>
2023-03-12 19:27:41 +0000

    typo: fixed a typo in the test for /health endpoint

--- server/src/controllers/health_controller/health_controller.test.ts
+++ server/src/controllers/health_controller/health_controller.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest';
 import { app } from '../../app';
 
 describe('GET /api/v1/health endpoint', () => {
-    it('Should', async () => {
+    it('Should check the health of the api', async () => {
         const res = await request(app).get('/api/v1/health');
         expect(res.statusCode).toEqual(200);
         expect(res.text).toEqual('Welcome to the Movie API!');
