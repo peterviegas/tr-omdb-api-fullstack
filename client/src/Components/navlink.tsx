e63ac6d7b5f4a3c58b7688fcda87edd428431cf0
neema <neemadas@gmail.com>
2023-03-12 12:55:29 +0000

    feat: Add hamburger for small devices

--- client/src/Components/navlink.tsx
+++ client/src/Components/navlink.tsx
@@ -3,57 +3,60 @@ import { NavLink } from 'react-router-dom';
 
 const ComponentNavlink: React.FC = () => {
     return (
-        <nav className='nav'>
-            <ul className='nav__list'>
-                <li className='nav__item'>
-                    <NavLink
-                        to='/'
-                        className={({ isActive }) =>
-                            isActive ? 'link--active' : 'link'
-                        }
-                    >
-                        {' '}
-                        Home{' '}
-                    </NavLink>
-                </li>
+        <>
+            <nav className='nav--smalldevices'></nav>
+            <nav className='nav'>
+                <ul className='nav__list'>
+                    <li className='nav__item'>
+                        <NavLink
+                            to='/'
+                            className={({ isActive }) =>
+                                isActive ? 'link--active' : 'link'
+                            }
+                        >
+                            {' '}
+                            Home{' '}
+                        </NavLink>
+                    </li>
 
-                <li className='nav__item'>
-                    <NavLink
-                        to='/movielist'
-                        className={({ isActive }) =>
-                            isActive ? 'link--active' : 'link'
-                        }
-                    >
-                        {' '}
-                        MoviesList{' '}
-                    </NavLink>
-                </li>
+                    <li className='nav__item'>
+                        <NavLink
+                            to='/movielist'
+                            className={({ isActive }) =>
+                                isActive ? 'link--active' : 'link'
+                            }
+                        >
+                            {' '}
+                            MoviesList{' '}
+                        </NavLink>
+                    </li>
 
-                <li className='nav__item'>
-                    <NavLink
-                        to='/about'
-                        className={({ isActive }) =>
-                            isActive ? 'link--active' : 'link'
-                        }
-                    >
-                        {' '}
-                        About{' '}
-                    </NavLink>
-                </li>
+                    <li className='nav__item'>
+                        <NavLink
+                            to='/about'
+                            className={({ isActive }) =>
+                                isActive ? 'link--active' : 'link'
+                            }
+                        >
+                            {' '}
+                            About{' '}
+                        </NavLink>
+                    </li>
 
-                <li className='nav__item'>
-                    <NavLink
-                        to='/team'
-                        className={({ isActive }) =>
-                            isActive ? 'link--active' : 'link'
-                        }
-                    >
-                        {' '}
-                        Team{' '}
-                    </NavLink>
-                </li>
-            </ul>
-        </nav>
+                    <li className='nav__item'>
+                        <NavLink
+                            to='/team'
+                            className={({ isActive }) =>
+                                isActive ? 'link--active' : 'link'
+                            }
+                        >
+                            {' '}
+                            Team{' '}
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </>
     );
 };
 
