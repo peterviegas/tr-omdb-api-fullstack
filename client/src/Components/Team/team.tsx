ce6333944b9d161a9a7df49cbab9c542c0561d80
neema <neemadas@gmail.com>
2023-03-12 03:39:38 +0000

    style: Add styles to home page

--- client/src/Components/Team/team.tsx
+++ client/src/Components/Team/team.tsx
@@ -1,29 +1,21 @@
-import React from "react";
-import "./team.css";
-import ComponentTeamPage from "./team_page";
+import React from 'react';
+import ComponentTeamPage from './team_page';
 
 const ComponentTeam: React.FC = () => (
+    <>
+        <div className='componentBody'>
+            <h2 className='text__center'> Our Team </h2>
 
-    < >
+            <p className='text__center'>
+                We are honest and dedicated Full-Stack Developers who love to
+                code and contribute to the World.
+            </p>
 
-        <div className="componentBody">
-
-            <h2 className="text__center"> Our Team </h2>
-
-            <p className="text__center"> We are honest and dedicated Full-Stack Developers who love to code and contribute to the World. </p>
-
-            <h3 className="text__center"> Meet The Team </h3>
+            <h3 className='text__center'> Meet The Team </h3>
 
             <ComponentTeamPage />
-
         </div>
-
-
-
-
-
     </>
-
 );
 
 export default ComponentTeam;
