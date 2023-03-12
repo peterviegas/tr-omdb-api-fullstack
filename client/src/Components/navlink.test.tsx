a4de52c8f25e71fdc2c784bfef25db79523f7d56
Peter Viegas <peterviegas@outlook.com>
2023-03-12 08:47:33 +0000

    Created component navlink.test

---
+++ client/src/Components/navlink.test.tsx
@@ -0,0 +1,114 @@
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import ComponentNavlink from "../Components/navlink";
+import Header from "../Components/Header/header"
+
+describe("Testing for the Navlink Component Home", () => {
+	test("renders Home", () => {
+		render(
+			<BrowserRouter>
+				<Header />
+			</BrowserRouter>
+		);
+		const titleElement=screen.getByText(/Home/i);
+		expect(titleElement).toBeInTheDocument();
+	});
+	
+	test("renders Home direct ComponentNavlink", () => {
+		render(
+			<BrowserRouter>
+				<ComponentNavlink />
+			</BrowserRouter>
+		);
+		const titleElement=screen.getByText(/Home/i);
+		expect(titleElement).toBeInTheDocument();
+	});
+});
+
+describe("Testing for the Navlink Component MoviesList", () => {
+	test("renders MoviesList", () => {
+		render(
+			<BrowserRouter>
+				<Header />
+			</BrowserRouter>
+		);
+		const titleElement=screen.getByText(/MoviesList/i);
+		expect(titleElement).toBeInTheDocument();
+	});
+	
+	test("renders MoviesList direct ComponentNavlink", () => {
+		render(
+			<BrowserRouter>
+				<ComponentNavlink />
+			</BrowserRouter>
+		);
+		const titleElement=screen.getByText(/MoviesList/i);
+		expect(titleElement).toBeInTheDocument();
+	});
+});
+
+describe("Testing for the Navlink Component Movie", () => {
+	test("renders Movie", () => {
+		render(
+			<BrowserRouter>
+				<Header />
+			</BrowserRouter>
+		);
+		const titleElement=screen.getByText('Movie');
+		expect(titleElement).toBeInTheDocument();
+	});
+	
+	test("renders Movie direct ComponentNavlink", () => {
+		render(
+			<BrowserRouter>
+				<ComponentNavlink />
+			</BrowserRouter>
+		);
+		const titleElement=screen.getByText('Movie');
+		expect(titleElement).toBeInTheDocument();
+	});
+});
+
+describe("Testing for the Navlink Component About", () => {
+	test("renders About", () => {
+		render(
+			<BrowserRouter>
+				<Header />
+			</BrowserRouter>
+		);
+		const titleElement=screen.getByText(/About/i);
+		expect(titleElement).toBeInTheDocument();
+	});
+	
+	test("renders About direct ComponentNavlink", () => {
+		render(
+			<BrowserRouter>
+				<ComponentNavlink />
+			</BrowserRouter>
+		);
+		const titleElement=screen.getByText(/About/i);
+		expect(titleElement).toBeInTheDocument();
+	});
+});
+
+describe("Testing for the Navlink Component Team", () => {
+	test("renders Team", () => {
+		render(
+			<BrowserRouter>
+				<Header />
+			</BrowserRouter>
+		);
+		const titleElement=screen.getByText(/Team/i);
+		expect(titleElement).toBeInTheDocument();
+	});
+	
+	test("renders Team direct ComponentNavlink", () => {
+		render(
+			<BrowserRouter>
+				<ComponentNavlink />
+			</BrowserRouter>
+		);
+		const titleElement=screen.getByText(/Team/i);
+		expect(titleElement).toBeInTheDocument();
+	});
+});
