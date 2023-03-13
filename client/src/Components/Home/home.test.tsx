151f15a121e5cebabf3cda3a68b0403c8e94f54b
Peter Viegas <peterviegas@outlook.com>
2023-03-13 19:20:05 +0000

    Change in component home.test - Adequacy to change component home.

--- client/src/Components/Home/home.test.tsx
+++ client/src/Components/Home/home.test.tsx
@@ -1,18 +1,17 @@
 import { render, screen } from "@testing-library/react";
 import ComponentHome from "./home";
+import { BrowserRouter } from "react-router-dom";
 
 
 describe("Testing for the Home Component Label", () => {
-
     test(`When the component is rendered, then the Home Component must be present`, async () => {
-
-        render(<ComponentHome />);
-
-        const testHome = screen.getByText(/welcome/i);
-
+        render(
+            <BrowserRouter>
+                <ComponentHome />
+            </BrowserRouter>
+        );
+        const testHome = screen.getByText(/Welcome to/i);
+        console.log("home", testHome)
         expect(testHome).toBeInTheDocument();
-
     });
-
-}
-);
+});
