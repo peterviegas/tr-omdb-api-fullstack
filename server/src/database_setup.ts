58c03ca739f9d1355d951c44b0e43e6eddfb6b91
glen <glen_dovey@hotmail.com>
2023-03-14 14:20:28 +0000

    feat: db setup

---
+++ server/src/database_setup.ts
@@ -0,0 +1,61 @@
+import { Client } from 'pg';
+import * as dotenv from 'dotenv';
+dotenv.config({ path: `.env.local` });
+
+const {
+    DB_HOST,
+    DB_PORT,
+    DB_ROOT_USER,
+    DB_ROOT_PASSWORD,
+    DB_USER,
+    DB_PASSWORD,
+    DB_DATABASE,
+    DB_TABLE,
+} = process.env;
+
+const main = async () => {
+    const client = new Client({
+        host: DB_HOST,
+        port: parseInt(DB_PORT as string),
+        user: DB_ROOT_USER,
+        password: DB_ROOT_PASSWORD,
+    });
+    await client.connect();
+    await client.query(`CREATE ROLE ${DB_USER} WITH
+        LOGIN
+        NOSUPERUSER
+        NOCREATEDB
+        NOCREATEROLE
+        INHERIT
+        NOREPLICATION
+        CONNECTION LIMIT -1
+        PASSWORD '${DB_PASSWORD}';`);
+    await client.query(`CREATE DATABASE ${DB_DATABASE}
+        WITH
+        OWNER = ${DB_USER}
+        ENCODING = 'UTF8'
+        CONNECTION LIMIT = -1
+        IS_TEMPLATE = False;`);
+    await client.end();
+    const db = new Client({
+        host: DB_HOST,
+        port: parseInt(DB_PORT as string),
+        user: DB_USER,
+        password: DB_PASSWORD,
+        database: DB_DATABASE,
+    });
+    await db.connect();
+    await db.query(`CREATE TABLE public.${DB_TABLE}
+        (
+            id integer NOT NULL,
+            movie_id TEXT UNIQUE,
+            PRIMARY KEY (id)
+        );`);
+    await db.query(`ALTER TABLE IF EXISTS public.${DB_TABLE}
+        OWNER to ${DB_USER};`);
+    await db.end();
+    console.log('Success');
+    return;
+};
+
+main();
