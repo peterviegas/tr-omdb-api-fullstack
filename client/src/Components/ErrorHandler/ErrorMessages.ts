7715d0cce6374ca829532d49d0386073be774aaa
neema <neemadas@gmail.com>
2023-03-15 05:34:43 +0000

    chore: Tidy up error messages

--- client/src/Components/ErrorHandler/ErrorMessages.ts
+++ client/src/Components/ErrorHandler/ErrorMessages.ts
@@ -1,7 +1,6 @@
 // Error messages based on response from API
 export const ErrorMessagesAPI = {
-    error404: '⛔️ Error: 404 URL not found!',
-    error500: '⛔️ Movie Not Found!',
-    errorFetch: '⛔️ Error: Something went wrong in fetching data!',
-    errorPost: '⛔️ Error: Something went wrong in posting data!',
+    error400: '⛔️ Movie not found!',
+    error500: '⛔️ Unexpected error while processing request',
+    errorFetch: '⛔️ Something went wrong in fetching data ',
 }
