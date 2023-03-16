54500ff601eba96095141e2c5e1df3c629e05eda
Peter Viegas <peterviegas@outlook.com>
2023-03-16 10:15:41 +0000

    Created component mainlayout.tst - Check component navigation Component Header Component Footer

---
+++ client/src/Components/mainlayout.test.tsx
@@ -0,0 +1,47 @@
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { ComponentMainlayout } from './mainlayout';
+
+describe('Testing for the ComponentMainlayout - - navigating to the component ComponentHeader', () => {
+    test(`When the component is rendered, then the Home option must be present`, async () => {
+        render(
+            <BrowserRouter>
+                <ComponentMainlayout />
+            </BrowserRouter>
+        );
+        const testHome = screen.getByText(/Home/i);
+        expect(testHome).toBeInTheDocument();
+    });
+
+	test(`When the component is rendered, then the Wishlist option must be present`, async () => {
+        render(
+            <BrowserRouter>
+                <ComponentMainlayout />
+            </BrowserRouter>
+        );
+        const testWishlist = screen.getByText(/Wishlist/i);
+        expect(testWishlist).toBeInTheDocument();
+    });
+
+	test(`When the component is rendered, then the About option must be present`, async () => {
+        render(
+            <BrowserRouter>
+                <ComponentMainlayout />
+            </BrowserRouter>
+        );
+        const testAbout = screen.getByText(/About/i);
+        expect(testAbout).toBeInTheDocument();
+    });
+});
+
+describe('Testing for the ComponentMainlayout - - navigating to the component ComponentFooter', () => {
+    test(`When the component is rendered, then the Home option must be present`, async () => {
+        render(
+            <BrowserRouter>
+                <ComponentMainlayout />
+            </BrowserRouter>
+        );
+        const testFooter = screen.getByText(/© 2023 PNGC/i);
+        expect(testFooter).toBeInTheDocument();
+    });
+});
