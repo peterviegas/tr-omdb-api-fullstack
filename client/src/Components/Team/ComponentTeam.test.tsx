f0f3a0d7a9739b341d31e2cea0be8ffd1642e14c
neema <neemadas@gmail.com>
2023-03-16 07:39:32 +0000

    chore: Tidy up folders

---
+++ client/src/Components/Team/ComponentTeam.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from '@testing-library/react';
+import ComponentTeam from './ComponentTeam';
+
+describe('Testing for the Team Component Label', () => {
+    test(`When the component is rendered, then the Team Component must be present`, async () => {
+        render(<ComponentTeam />);
+
+        const testHome = screen.getByText(/Our Team/i);
+
+        expect(testHome).toBeInTheDocument();
+    });
+
+    test(`When the component is rendered, then the honest Component must be present`, async () => {
+        render(<ComponentTeam />);
+
+        const testHome = screen.getByText(
+            /We are honest and dedicated Full-Stack Developers who love to code and contribute to the World./i
+        );
+
+        expect(testHome).toBeInTheDocument();
+    });
+});
