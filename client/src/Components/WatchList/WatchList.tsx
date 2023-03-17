0316d973611d67e1d130a5894b61f74174aa3d4a
neema <neemadas@gmail.com>
2023-03-17 06:18:53 +0000

    feat: Add watchlist feature

---
+++ client/src/Components/WatchList/WatchList.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import RenderMoviesList from '../Movie/RenderMoviesList';
+
+const WatchList: React.FC = () => {
+    return (
+        <div className='watchlist'>
+            {/* <RenderMoviesList movies={} searchedMovieName={''} onClick={} /> */}
+        </div>
+    );
+};
+
+export default WatchList;
