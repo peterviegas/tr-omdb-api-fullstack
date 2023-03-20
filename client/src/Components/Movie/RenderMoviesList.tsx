ef5374227a829a3f3a48319a52165051005a6877
neema <neemadas@gmail.com>
2023-03-20 03:05:34 +0000

    feat: Add cannot find similar movies error message

--- client/src/Components/Movie/RenderMoviesList.tsx
+++ client/src/Components/Movie/RenderMoviesList.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import ErrorComponent from '../ErrorHandler/ErrorComponent';
 import { MovieType } from './Type/MovieType.type';
+import { ErrorMessagesAPI } from '../ErrorHandler/ErrorMessages';
+const { errorMovie } = ErrorMessagesAPI;
 const noImage = require('../../Images/noImage.jpg');
 
 interface RenderMoviesListProp {
@@ -16,30 +19,36 @@ const RenderMoviesList: React.FC<RenderMoviesListProp> = ({
 }) => {
     return (
         <>
-            {movies.map((movie) => {
-                const { Title, Year, imdbID, Poster } = movie;
-                return (
-                    <div
-                        key={imdbID}
-                        className='movieList__item'
-                        onClick={onClick}
-                    >
-                        <NavLink
-                            to={`/movie/${searchedMovieName}/${imdbID}`}
-                            className='movieList__link'
+            {movies.length === 0 ? (
+                <ErrorComponent>{errorMovie}</ErrorComponent>
+            ) : (
+                movies.map((movie) => {
+                    const { Title, Year, imdbID, Poster } = movie;
+                    return (
+                        <div
+                            key={imdbID}
+                            className='movieList__item'
+                            onClick={onClick}
                         >
-                            <div className='movieList__img-container'>
-                                <img
-                                    className='movieList__img'
-                                    src={Poster === 'N/A' ? noImage : Poster}
-                                    alt={Title}
-                                />
-                            </div>
-                            <h6 className='movieList__heading'>{`${Title}, ${Year}`}</h6>
-                        </NavLink>
-                    </div>
-                );
-            })}
+                            <NavLink
+                                to={`/movie/${searchedMovieName}/${imdbID}`}
+                                className='movieList__link'
+                            >
+                                <div className='movieList__img-container'>
+                                    <img
+                                        className='movieList__img'
+                                        src={
+                                            Poster === 'N/A' ? noImage : Poster
+                                        }
+                                        alt={Title}
+                                    />
+                                </div>
+                                <h6 className='movieList__heading'>{`${Title}, ${Year}`}</h6>
+                            </NavLink>
+                        </div>
+                    );
+                })
+            )}
         </>
     );
 };
